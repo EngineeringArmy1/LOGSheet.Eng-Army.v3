98c5d9dd76a7dc7da938826b0cd049e29127621c
EngineeringArmy1 <155462254+EngineeringArmy1@users.noreply.github.com>
2024-01-03 18:43:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('loginButton').addEventListener('mouseenter', function() {
+    document.getElementById('loginContainer').style.backgroundColor = '#2980b9';
+  });
+  
+  document.getElementById('loginButton').addEventListener('mouseleave', function() {
+    document.getElementById('loginContainer').style.backgroundColor = 'rgba(255, 255, 255, 0.9)';
+  });
+  
